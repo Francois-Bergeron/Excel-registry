a4cee4d90cce1b121cbcda2860bb099c07653e94
Francois-Bergeron <frankyboy_89@hotmail.com>
2020-08-17 22:12:28 -0400

    Forced overwrite - faster

--- Excel_ContextMenu.bat
+++ Excel_ContextMenu.bat
@@ -13,49 +13,49 @@ IF /I "%confirmation%" NEQ "I" GOTO UNINSTALL
 :: Install
 :: XLSX
 :: Right-click open as read-only
-reg add "HKCU\Software\Classes\Excel.Sheet.12\shell\open as read-only" /ve /t REG_SZ /d "Open as Read-Only"
-reg add "HKCU\Software\Classes\Excel.Sheet.12\shell\open as read-only" /v "Icon" /t REG_SZ /d "\"C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE\""
-reg add "HKCU\Software\Classes\Excel.Sheet.12\shell\open as read-only\command" /ve /t REG_SZ /d "\"C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE\" /r \"%1\""
+reg add "HKCU\Software\Classes\Excel.Sheet.12\shell\open as read-only" /ve /t REG_SZ /d "Open as Read-Only" /f
+reg add "HKCU\Software\Classes\Excel.Sheet.12\shell\open as read-only" /v "Icon" /t REG_SZ /d "\"C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE\"" /f
+reg add "HKCU\Software\Classes\Excel.Sheet.12\shell\open as read-only\command" /ve /t REG_SZ /d "\"C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE\" /r \"%1\"" /f
 :: Right-click open as new instance
-reg add "HKCU\Software\Classes\Excel.Sheet.12\shell\open as new instance" /ve /t REG_SZ /d "Open as New Instance"
-reg add "HKCU\Software\Classes\Excel.Sheet.12\shell\open as new instance" /v "Icon" /t REG_SZ /d "\"C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE\""
-reg add "HKCU\Software\Classes\Excel.Sheet.12\shell\open as new instance\command" /ve /t REG_SZ /d "\"C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE\" /x \"%1\""
+reg add "HKCU\Software\Classes\Excel.Sheet.12\shell\open as new instance" /ve /t REG_SZ /d "Open as New Instance" /f
+reg add "HKCU\Software\Classes\Excel.Sheet.12\shell\open as new instance" /v "Icon" /t REG_SZ /d "\"C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE\"" /f
+reg add "HKCU\Software\Classes\Excel.Sheet.12\shell\open as new instance\command" /ve /t REG_SZ /d "\"C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE\" /x \"%1\"" /f
 :: XLSM
 :: Right-click open as read-only
-reg add "HKCU\Software\Classes\Excel.SheetMacroEnabled.12\shell\open as read-only" /ve /t REG_SZ /d "Open as Read-Only"
-reg add "HKCU\Software\Classes\Excel.SheetMacroEnabled.12\shell\open as read-only" /v "Icon" /t REG_SZ /d "\"C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE\""
-reg add "HKCU\Software\Classes\Excel.SheetMacroEnabled.12\shell\open as read-only\command" /ve /t REG_SZ /d "\"C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE\" /r \"%1\""
+reg add "HKCU\Software\Classes\Excel.SheetMacroEnabled.12\shell\open as read-only" /ve /t REG_SZ /d "Open as Read-Only" /f
+reg add "HKCU\Software\Classes\Excel.SheetMacroEnabled.12\shell\open as read-only" /v "Icon" /t REG_SZ /d "\"C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE\"" /f
+reg add "HKCU\Software\Classes\Excel.SheetMacroEnabled.12\shell\open as read-only\command" /ve /t REG_SZ /d "\"C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE\" /r \"%1\"" /f
 :: Right-click open as new instance
-reg add "HKCU\Software\Classes\Excel.SheetMacroEnabled.12\shell\open as new instance" /ve /t REG_SZ /d "Open as New Instance"
-reg add "HKCU\Software\Classes\Excel.SheetMacroEnabled.12\shell\open as new instance" /v "Icon" /t REG_SZ /d "\"C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE\""
-reg add "HKCU\Software\Classes\Excel.SheetMacroEnabled.12\shell\open as new instance\command" /ve /t REG_SZ /d "\"C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE\" /x \"%1\""
+reg add "HKCU\Software\Classes\Excel.SheetMacroEnabled.12\shell\open as new instance" /ve /t REG_SZ /d "Open as New Instance" /f
+reg add "HKCU\Software\Classes\Excel.SheetMacroEnabled.12\shell\open as new instance" /v "Icon" /t REG_SZ /d "\"C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE\"" /f
+reg add "HKCU\Software\Classes\Excel.SheetMacroEnabled.12\shell\open as new instance\command" /ve /t REG_SZ /d "\"C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE\" /x \"%1\"" /f
 :: XLSB
 :: Right-click open as read-only
-reg add "HKCU\Software\Classes\Excel.SheetBinaryMacroEnabled.12\shell\open as read-only" /ve /t REG_SZ /d "Open as Read-Only"
-reg add "HKCU\Software\Classes\Excel.SheetBinaryMacroEnabled.12\shell\open as read-only" /v "Icon" /t REG_SZ /d "\"C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE\""
-reg add "HKCU\Software\Classes\Excel.SheetBinaryMacroEnabled.12\shell\open as read-only\command" /ve /t REG_SZ /d "\"C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE\" /r \"%1\""
+reg add "HKCU\Software\Classes\Excel.SheetBinaryMacroEnabled.12\shell\open as read-only" /ve /t REG_SZ /d "Open as Read-Only" /f
+reg add "HKCU\Software\Classes\Excel.SheetBinaryMacroEnabled.12\shell\open as read-only" /v "Icon" /t REG_SZ /d "\"C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE\"" /f
+reg add "HKCU\Software\Classes\Excel.SheetBinaryMacroEnabled.12\shell\open as read-only\command" /ve /t REG_SZ /d "\"C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE\" /r \"%1\"" /f
 :: Right-click open as new instance
-reg add "HKCU\Software\Classes\Excel.SheetBinaryMacroEnabled.12\shell\open as new instance" /ve /t REG_SZ /d "Open as New Instance"
-reg add "HKCU\Software\Classes\Excel.SheetBinaryMacroEnabled.12\shell\open as new instance" /v "Icon" /t REG_SZ /d "\"C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE\""
-reg add "HKCU\Software\Classes\Excel.SheetBinaryMacroEnabled.12\shell\open as new instance\command" /ve /t REG_SZ /d "\"C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE\" /x \"%1\""
+reg add "HKCU\Software\Classes\Excel.SheetBinaryMacroEnabled.12\shell\open as new instance" /ve /t REG_SZ /d "Open as New Instance" /f
+reg add "HKCU\Software\Classes\Excel.SheetBinaryMacroEnabled.12\shell\open as new instance" /v "Icon" /t REG_SZ /d "\"C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE\"" /f
+reg add "HKCU\Software\Classes\Excel.SheetBinaryMacroEnabled.12\shell\open as new instance\command" /ve /t REG_SZ /d "\"C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE\" /x \"%1\"" /f
 :: XLS
 :: Right-click open as read-only
-reg add "HKCU\Software\Classes\Excel.Sheet.8\shell\open as read-only" /ve /t REG_SZ /d "Open as Read-Only"
-reg add "HKCU\Software\Classes\Excel.Sheet.8\shell\open as read-only" /v "Icon" /t REG_SZ /d "\"C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE\""
-reg add "HKCU\Software\Classes\Excel.Sheet.8\shell\open as read-only\command" /ve /t REG_SZ /d "\"C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE\" /r \"%1\""
+reg add "HKCU\Software\Classes\Excel.Sheet.8\shell\open as read-only" /ve /t REG_SZ /d "Open as Read-Only" /f
+reg add "HKCU\Software\Classes\Excel.Sheet.8\shell\open as read-only" /v "Icon" /t REG_SZ /d "\"C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE\"" /f
+reg add "HKCU\Software\Classes\Excel.Sheet.8\shell\open as read-only\command" /ve /t REG_SZ /d "\"C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE\" /r \"%1\"" /f
 :: Right-click open as new instance
-reg add "HKCU\Software\Classes\Excel.Sheet.8\shell\open as new instance" /ve /t REG_SZ /d "Open as New Instance"
-reg add "HKCU\Software\Classes\Excel.Sheet.8\shell\open as new instance" /v "Icon" /t REG_SZ /d "\"C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE\""
-reg add "HKCU\Software\Classes\Excel.Sheet.8\shell\open as new instance\command" /ve /t REG_SZ /d "\"C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE\" /x \"%1\""
+reg add "HKCU\Software\Classes\Excel.Sheet.8\shell\open as new instance" /ve /t REG_SZ /d "Open as New Instance" /f
+reg add "HKCU\Software\Classes\Excel.Sheet.8\shell\open as new instance" /v "Icon" /t REG_SZ /d "\"C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE\"" /f
+reg add "HKCU\Software\Classes\Excel.Sheet.8\shell\open as new instance\command" /ve /t REG_SZ /d "\"C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE\" /x \"%1\"" /f
 :: CSV
 :: Right-click open as read-only
-reg add "HKCU\Software\Classes\Excel.CSV\shell\open as read-only" /ve /t REG_SZ /d "Open as Read-Only"
-reg add "HKCU\Software\Classes\Excel.CSV\shell\open as read-only" /v "Icon" /t REG_SZ /d "\"C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE\""
-reg add "HKCU\Software\Classes\Excel.CSV\shell\open as read-only\command" /ve /t REG_SZ /d "\"C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE\" /r \"%1\""
+reg add "HKCU\Software\Classes\Excel.CSV\shell\open as read-only" /ve /t REG_SZ /d "Open as Read-Only" /f
+reg add "HKCU\Software\Classes\Excel.CSV\shell\open as read-only" /v "Icon" /t REG_SZ /d "\"C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE\"" /f
+reg add "HKCU\Software\Classes\Excel.CSV\shell\open as read-only\command" /ve /t REG_SZ /d "\"C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE\" /r \"%1\"" /f
 :: Right-click open as new instance
-reg add "HKCU\Software\Classes\Excel.CSV\shell\open as new instance" /ve /t REG_SZ /d "Open as New Instance"
-reg add "HKCU\Software\Classes\Excel.CSV\shell\open as new instance" /v "Icon" /t REG_SZ /d "\"C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE\""
-reg add "HKCU\Software\Classes\Excel.CSV\shell\open as new instance\command" /ve /t REG_SZ /d "\"C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE\" /x \"%1\""
+reg add "HKCU\Software\Classes\Excel.CSV\shell\open as new instance" /ve /t REG_SZ /d "Open as New Instance" /f
+reg add "HKCU\Software\Classes\Excel.CSV\shell\open as new instance" /v "Icon" /t REG_SZ /d "\"C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE\"" /f
+reg add "HKCU\Software\Classes\Excel.CSV\shell\open as new instance\command" /ve /t REG_SZ /d "\"C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE\" /x \"%1\"" /f
 
 
 echo ************************************************************************
